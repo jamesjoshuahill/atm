b475e181573f6852fb75390627aeaff2900e36c1
Josh Hill <jamesjoshuahill@gmail.com>
2013-09-02 11:27:16 +0100

    Used double to stand in as a generic User

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -3,7 +3,7 @@ require 'bank'
 describe Bank do
   it 'can open an account' do
     bank = Bank.new
-    user = User.new('Joe', 'Bloggs', '1234')
+    user = double User
     bank.open_account(user)
 
     expect(bank.accounts).to include user
